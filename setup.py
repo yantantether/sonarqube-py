91beb0b981e2158fe7ee8e95220d73fa67ca6321
Pat <patrick.turner@nhs.net>
2021-10-05 12:49:32 +0100

    sorting deployment keys

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sonarqube-py",
-    version="0.3.0",
+    version="0.3.1",
     author="Pat Turner",
     description="A python wrapper for the SonarQube web API",
     long_description=long_description,
