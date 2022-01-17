7daee31a683aca869b32658f90536b213627dcf9
Pat <patrick.turner@nhs.net>
2022-01-17 19:55:27 +0000

    Added CLI to manage SonarQube projects
    add/delete projects and their quality gates and profiles

---
+++ sonarqube/cli.py
@@ -0,0 +1,64 @@
+import click
+import logging
+from requests import RequestException
+from os import path
+import yaml
+from .api import SonarQube
+from .community import Project
+from .exceptions import CliException
+
+# logging
+logger = logging.getLogger()
+
+pass_config = click.make_pass_decorator(Project, ensure=True)
+
+@click.group()
+@click.option('-c', 'config', help='Configuration file')
+@click.option('-s', 'suffix', help='Suffix to use when managing SonarQube projects')
+@click.option('-h', 'host', help='SonarQube host')
+@click.option('-p', 'port', help='SonarQube port')
+@click.option('-t', 'token', help='SonarQube personal access token')
+@click.option('-l', 'log_level', help='Logging level')
+@click.pass_context
+def cli(ctx, config=None, suffix=None, host=None, port=None, token=None, log_level=None):
+    logging.basicConfig(level=log_level or logging.INFO)
+    sq = SonarQube(host = host, port = port, token = token)
+    ctx.obj = read_project(file=config, suffix=suffix, sq = sq)
+
+@cli.command()
+@pass_config
+def create(project):
+    try:
+        project.create_or_update()
+    except RequestException as e:
+        logger.error("Status [%s]: %s", e.response.status_code, e)
+
+@cli.command()
+@pass_config
+def delete(project):
+    try:
+        project.delete()
+    except RequestException as e:
+        logger.error("Status [%s]: %s", e.response.status_code, e)
+
+def read_project(file = None, suffix = None, sq = None):
+    config_filename = file or ".sonarqube.yml"
+    if (not path.isfile(config_filename)):
+        raise CliException("sonarqube config file missing: [%s]".format(config_filename))
+    with open(config_filename, "r") as stream:
+        try:
+            config = yaml.safe_load(stream)
+            config_project = config['project']
+            return Project(
+                key = config_project['key'],
+                name = config_project['name'],
+                suffix = suffix,
+                quality_gate = config_project['quality-gate'],
+                quality_profiles = config_project['quality-profiles'] or {},
+                sq = sq
+            )
+        except yaml.YAMLError as exc:
+            raise CliException("Failed to parse config file [%s] %s".format(config_filename, exc))
+
+if __name__ == "__main__":
+    cli()
--- sonarqube/community.py
+++ sonarqube/community.py
@@ -14,36 +14,40 @@ from .api import SonarQube
 import logging
 
 logger = logging.getLogger('sonarqube.community')
-
+       
 class Project(object):
     
-    DEFAULT_SUFFIX = 'release'
+    DEFAULT_KEY_DELIMITER = ':'
     DEFAULT_NAME_DELIMITER = ' '
     
-    def __init__(self, key, name, suffix=None, quality_gate=None, quality_profiles=None, name_delimiter=None, sq=None) -> None:
+    def __init__(self, key, name, suffix=None, quality_gate=None, quality_profiles=None,
+                 key_delimiter=None, name_delimiter=None, sq=None) -> None:
         super().__init__()
         self.key = key
         self.name = name
-        self.suffix = suffix or Project.DEFAULT_SUFFIX
+        self.suffix = self.sanitise(suffix)
         self.quality_gate = quality_gate
         self.quality_profiles = quality_profiles
-        self.name_delimiter = name_delimiter or Project.DEFAULT_NAME_DELIMITER
-        self.full_key = '{}:{}'.format(self.key, self.suffix)
-        self.full_name = '{}{}{}'.format(self.name, self.name_delimiter, self.suffix)
+        self.full_key = self.format(self.key, key_delimiter or Project.DEFAULT_KEY_DELIMITER, self.suffix)
+        self.full_name = self.format(self.name, name_delimiter or Project.DEFAULT_NAME_DELIMITER, self.suffix)
         self.sq = sq or SonarQube()
         self.sq_project = None
         self.sq_quality_profiles = None
     
     def read(self):
         self.sq_project = next(self.sq.get_projects_search(projects=self.full_key), None)
+        if (self.sq_project):
+            logger.info(f"Retrieved project [{self.full_key}] from SonarQube")
 
     def create_or_update(self):
         if (not self.sq_project):
             self.read()
         if (not self.sq_project):
+            logger.info(f"Creating project [{self.full_key}]")
             self.sq.post_projects_create(project=self.full_key, name=self.full_name)
             self.read()
         if (self.quality_gate):
+            logger.info(f"Assigning quality gate [{self.quality_gate}] to [{self.full_key}]")
             self.sq.post_qualitygates_select(gateName=self.quality_gate, projectKey=self.full_key)
         if (self.quality_profiles):
             self._assign_quality_profiles()
@@ -55,8 +59,16 @@ class Project(object):
             self.sq.post_projects_delete(project=self.full_key)
             self.read()
 
+    def sanitise(self, value):
+        # handling 'feature/branch' case for now
+        # consider more sophisticated cleanup
+        return value.replace('/', '-') if value else None
+        
+    def format(self, prefix, delimiter, suffix):
+        return prefix if not suffix else f'{prefix}{delimiter}{suffix}'
+        
     def _assign_quality_profiles(self):
         for lang in self.quality_profiles.keys():
             profile = self.quality_profiles[lang]
-            logger.info("Adding profile [%s] for language [%s]", profile, lang)
+            logger.info(f"Assigning quality profile [{profile}] for language [{lang}] to [{self.full_key}]")
             self.sq.post_qualityprofiles_add_project(language = lang, project = self.full_key, qualityProfile = profile)
